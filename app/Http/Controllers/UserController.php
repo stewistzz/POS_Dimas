53e51e7f09e27c25d0b109fef49d06f92e3bb48d
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-03-16 12:52:30 +0700

    js4_praktikum2_Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 
 use Illuminate\Http\Request;
@@ -35,18 +36,23 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        // eloquent ORM
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // // eloquent ORM
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         // bagian 1
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        // modifikasi perubahan dari js 4
+        $user = UserModel::findOr(20, ['username','nama'], function() {
+            abort(404);
+        }) ;
         return view('user', ['data' => $user]);
     }
 }

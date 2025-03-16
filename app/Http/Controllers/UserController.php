bf3192119eee6fc697394550efcb63736a90872a
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-03-16 13:12:55 +0700

    js4_praktikum2.2_Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,10 +49,14 @@ class UserController extends Controller
         // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
 
+        // // modifikasi perubahan dari js 4
+        // $user = UserModel::findOr(20, ['username','nama'], function() {
+        //     abort(404);
+        // }) ;
+        // return view('user', ['data' => $user]);
+
         // modifikasi perubahan dari js 4
-        $user = UserModel::findOr(20, ['username','nama'], function() {
-            abort(404);
-        }) ;
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }

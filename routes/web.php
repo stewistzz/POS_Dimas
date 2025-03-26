46b396c50b2ed38a2029037ba29bef73b5804930
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-03-26 08:59:04 +0700

    JS06_Praktikum02_Modal Ajax Edit Data (Data User)

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,11 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+
+    // edit dan update dengan ajax
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);  // menyimpan perubahan data user
+
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 

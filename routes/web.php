94782d2836f56d558cf94c5edda87888ac3ebb8b
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-05-08 16:08:52 +0700

    js08_praktikum02_Export Data ke Excel

--- routes/web.php
+++ routes/web.php
@@ -172,6 +172,7 @@ Route::middleware('auth')->group(function () {
             // route baru untuk melakukan import
             Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
         });
     });
 });
